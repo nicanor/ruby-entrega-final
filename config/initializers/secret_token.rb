cf673d3f9cf74d484d73454fa83c8f38dced3eca
Nica <nicanorperera@gmail.com>
2014-05-28 14:35:05 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trabajofinal::Application.config.secret_key_base = '363657a8c3ec61b3745631f112569c5529cc7998cc6001b0e100e72001175e9b66a4a85afc4672904adcee67c2d5ecc2a71535289c269294acdfdd6cfa19f415'
---
+++ test/models/resource_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ResourceTest < ActiveSupport::TestCase
+  test "should fail" do
+    assert false
+  end
+end
