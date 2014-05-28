ec5ec79befb02678e1e74542a3c4098694c5aa08
Nica <nicanorperera@gmail.com>
2014-05-28 14:45:18 -0300

    Added Description to Resources.

--- test/models/resource_test.rb
+++ test/models/resource_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ResourceTest < ActiveSupport::TestCase
-  test "should fail" do
-    assert false
+  test "should create resource" do
+    x = Resource.count
+    r = Resource.create(name: 'Name', description: 'Description')
+    assert_equal (x+1), Resource.count
+    
+    assert_equal 'Name', r.name
+    assert_equal 'Description', r.description
   end
 end
