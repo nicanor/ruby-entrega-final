5f783a786c196901ade815c514a46a47b1710465
Nica <nicanorperera@gmail.com>
2014-05-28 15:05:56 -0300

    Added tests for update and destroy Resource. All are passing.

--- test/models/resource_test.rb
+++ test/models/resource_test.rb
@@ -9,4 +9,18 @@ class ResourceTest < ActiveSupport::TestCase
     assert_equal 'Name', r.name
     assert_equal 'Description', r.description
   end
+
+  test "should update resource" do
+    Resource.last.update(name: 'New Name')    
+    assert_equal 'New Name', Resource.last.name
+  end
+
+  test "should destroy resource" do
+    x = Resource.count
+    r = Resource.last
+    id = r.id
+    r.destroy
+    assert_equal (x-1), Resource.count
+    assert_raises(ActiveRecord::RecordNotFound) { Resource.find(id) }
+  end
 end
