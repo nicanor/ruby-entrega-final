5cf7c4eba4b48de47b7d9d2bd3d1cd87c5842220
Nica <nicanorperera@gmail.com>
2014-05-29 23:37:09 -0300

    Index.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,8 @@
 class ResourcesController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  respond_to :json
   def index
+    render json: Resource.all
   end
 end
--- test/controllers/resources_controller_test.rb
+++ test/controllers/resources_controller_test.rb
@@ -4,6 +4,11 @@ class ResourcesControllerTest < ActionController::TestCase
 
   test "should index resources" do
     assert_routing '/resources', {controller: "resources", action: "index"}
+    get :index
+    assert_response :success
+    data = JSON.parse @response.body
+    data.map! {|r| Resource.new(r)}
+    assert_equal Resource.all, data
   end
 
 end
